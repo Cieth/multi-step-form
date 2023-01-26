da3411930451617c2a5db62ce0488638a3ad51af
Omar Vega <omarvega598@gmail.com>
2023-01-25 22:04:13 -0500

    add navegation mockup

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-
+import styles from '../styles/components/Navigation.module.css';
 const Navigation = () => {
   return (
-    <>
+    <div className={styles.Navigation__body}>
       <button>1</button>
       <button>2</button>
       <button>3</button>
       <button>4</button>
-      <button>5</button>
-      <button>6</button>
-    </>
+    </div>
   );
-
 };
 
 export default Navigation;
